7377483bd5956e74f4818fd290e443eee15bdedf
Satyanarayan Yadav <satyanarayan.y@gmail.com>
2019-05-27 16:13:10 +0530

    Part-1
    Add very basic flow to show thymeleaf features

--- src/main/java/com/srysoft/redditDemo/controller/HomeController.java
+++ src/main/java/com/srysoft/redditDemo/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.srysoft.redditDemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	@GetMapping(value = { "/", "/home" })
-	public String home() {
-		return ("Welcome to Spring 2 Demo, Appliation!!");
+	public String home(Model model) {
+
+		model.addAttribute("title", "Home");
+		model.addAttribute("message", "Hello, Spring Boot Application with View Template Engine- Thymeleaf");
+		return "home";
 	}
 
 }
