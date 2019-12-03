daa8d6bb7c9d0df7a991d803e9d68cfbd2161fe3
Satyanarayan Yadav <satyanarayan.y@gmail.com>
2019-12-03 21:18:04 +0530

    Part-3 User Detail Service

--- src/main/java/com/srysoft/redditDemo/repository/UserRepository.java
+++ src/main/java/com/srysoft/redditDemo/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.srysoft.redditDemo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.srysoft.redditDemo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	Optional<User> findByEmail(String email);
+	
 }
