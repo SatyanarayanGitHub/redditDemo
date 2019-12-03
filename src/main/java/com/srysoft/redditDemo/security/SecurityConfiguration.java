bd7b66f5a6e3d7d14bf4b5df1b4e06c798f4a3ce
Satyanarayan Yadav <satyanarayan.y@gmail.com>
2019-12-03 21:37:14 +0530

    Part-4 Add Users and Roles to Database

--- src/main/java/com/srysoft/redditDemo/security/SecurityConfiguration.java
+++ src/main/java/com/srysoft/redditDemo/security/SecurityConfiguration.java
@@ -20,7 +20,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/link/submit").hasRole("ADMIN")
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/link/submit").hasRole("USER")
 				.antMatchers("/h2-console/**").permitAll().and().formLogin();
 
 	}
