0801a213cb95d9aa11075a2d6ca31a169aee1cc9
Satyanarayan Yadav <satyanarayan.y@gmail.com>
2019-12-04 12:23:35 +0530

    part-4 Thymeleaf Spring Security Dialect

--- src/main/java/com/srysoft/redditDemo/RedditDemoApplication.java
+++ src/main/java/com/srysoft/redditDemo/RedditDemoApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 public class RedditDemoApplication {
@@ -14,37 +16,15 @@ public class RedditDemoApplication {
 		log.info("==>> Application Start!!");
 		SpringApplication.run(RedditDemoApplication.class, args);
 	}
-	
-	/*
 
+	
+	// TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
+	// TODO   for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extras-springsecurity4)
 	@Bean
-	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository, HomeService homeService) {
-		return args -> {
-			log.info("<<-----------Command Line Runner ----------->>");
-			Link link = new Link("Spring Boot 2 Swagger", "https://dzone.com/articles/spring-boot-2-restful-api-documentation-with-swagg");
-			linkRepository.save(link);
-			
-
-			Comment comment = new Comment("Swagger doc, what a document", link);
-			commentRepository.save(comment);
-			link.addComment(comment);
-			
-			
-			Hello hello1 = new Hello("Satya", "Welcome! Satya");
-			Hello hello2 = new Hello("Ronak", "Welcome! Ronak");
-			Hello hello3 = new Hello("Nikhil", "Welcome! Nikhil");
-			Hello hello4 = new Hello("Kailash", "Welcome! Kailash");
-			
-			homeService.create(hello1);
-			homeService.create(hello2);
-			homeService.create(hello3);
-			homeService.create(hello4);
-			
-		
-		};
+	public SpringSecurityDialect securityDialect() {
+		return new SpringSecurityDialect();
 	}
-	
-	*/
+
 	/**
 	 * Reference Link 
 	 * 1. https://github.com/danvega/springit-course-docs 
